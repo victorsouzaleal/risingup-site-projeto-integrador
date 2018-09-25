f8ebdf1170030855a9bb6024ab119aa63ea5ddbb
Victorsouza02 <victorsouzaleal@gmail.com>
2018-09-25 12:27:00 -0300

    Nome do Projeto alterado

--- src/java/util/Arquivo.java
+++ src/java/util/Arquivo.java
@@ -10,11 +10,11 @@ import java.util.UUID;
 
 public class Arquivo {
     String path_foto_produto = System.getProperty("user.home") + ""
-                + "/Documents/NetBeansProjects/Loja_JavaEE/web/img/produtos";//windows
-        //+ "/NetBeansProjects/lomoj/web/img/clientes/";//linux
+                + "/Documents/NetBeansProjects/RisingUP/web/img/produtos";//windows
+        //+ "/NetBeansProjects/RisingUP/web/img/clientes/";//linux
     String path_icone_cat = System.getProperty("user.home") + ""
-                + "/Documents/NetBeansProjects/Loja_JavaEE/web/img/icones_cat";//windows
-        //+ "/NetBeansProjects/lomoj/web/img/clientes/";//linux
+                + "/Documents/NetBeansProjects/RisingUP/web/img/icones_cat";//windows
+        //+ "/NetBeansProjects/RisingUP/web/img/clientes/";//linux
 
     public String getPath_foto_produto() {
         return path_foto_produto;
