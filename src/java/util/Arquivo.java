89eec33d15c2ea00fb6b38feaed18acc2f2b4195
Victorsouza02 <victorsouzaleal@gmail.com>
2018-09-25 20:39:37 -0300

    Mudança nome projeto

--- src/java/util/Arquivo.java
+++ src/java/util/Arquivo.java
@@ -10,10 +10,10 @@ import java.util.UUID;
 
 public class Arquivo {
     String path_foto_produto = System.getProperty("user.home") + ""
-                + "/Documents/NetBeansProjects/RisingUP/web/img/produtos";//windows
+                + "/Documents/NetBeansProjects/RisingUP-Informatica/web/img/produtos";//windows
         //+ "/NetBeansProjects/RisingUP/web/img/clientes/";//linux
     String path_icone_cat = System.getProperty("user.home") + ""
-                + "/Documents/NetBeansProjects/RisingUP/web/img/icones_cat";//windows
+                + "/Documents/NetBeansProjects/RisingUP-Informatica/web/img/icones_cat";//windows
         //+ "/NetBeansProjects/RisingUP/web/img/clientes/";//linux
 
     public String getPath_foto_produto() {
